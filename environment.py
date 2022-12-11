c9d0eda175958aae77f52408473ad80aa90d64f5
José Pedro Carvalho <up201806263@fe.up.pt>
2022-12-11 17:19:26 +0000

    Refactoring Guru

--- environment.py
+++ environment.py
@@ -3,7 +3,7 @@ import random
 
 
 class BlackJack:
-    def __init__(self):
+    def __init__(self,type="normal"):
         self.p = 0
         self.d = 0
         self.cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
@@ -11,13 +11,17 @@ class BlackJack:
         self.dealer_ace = False
         self.terminated = False
         self.action_space_n = len(self.get_actions())
-        self.name="default"
+        self.type=type
+        self.base_reward=1
 
     def get_state(self):
         return (self.p, self.d, self.player_ace)
 
     def get_actions(self):
-        return [0, 1]
+        if self.type=="normal":
+            return [0, 1]
+        else:
+            return[0,1,2]
 
     def reset(self):
         self.p = self.draw("p")
@@ -26,86 +30,7 @@ class BlackJack:
         self.player_ace = False
         self.dealer_ace = False
         self.terminated = False
-        return self.get_state()
-
-    def draw(self, participant="p"):
-
-        value = random.choice(self.cards)
-        if value == 11:
-            if participant == "p":
-                self.player_ace = True
-            else:
-                self.dealer_ace = True
-        return value
-
-    def is_bust(self, participant="p"):
-        if participant == "p":
-            if self.p > 21:
-                if self.player_ace:
-                    self.p -= 10
-                    self.player_ace = False
-                else:
-                    return True
-        if participant == "d":
-            if self.d > 21:
-                if self.dealer_ace:
-                    self.d -= 10
-                    self.dealer_ace = False
-                else:
-                    return True
-        return False
-
-    def step(self, action):
-        reward = 0
-
-        if action == 1:
-            self.p += self.draw("p")
-            if self.is_bust("p"):
-                self.terminated = True
-                reward = -1
-            else:
-                self.terminated = False
-                reward = 0
-
-        elif action == 0:
-            self.terminated = True
-            while self.d < 17:
-                self.d += self.draw("d")
-            if self.p == self.d:
-                reward = 0
-            if self.p > self.d or self.is_bust("d"):
-                reward = 1
-            if self.p < self.d <= 21:
-                reward = -1
-        return self.get_state(), self.terminated, reward
-
-
-class BlackJackDouble:
-    def __init__(self):
-        self.p = 0
-        self.d = 0
-        self.cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-        self.player_ace = False
-        self.dealer_ace = False
-        self.terminated = False
-        self.action_space_n = len(self.get_actions())
-        self.base_reward = 1
-        self.name = "double"
-
-    def get_state(self):
-        return (self.p, self.d, self.player_ace)
-
-    def get_actions(self):
-        return [0, 1, 2]
-
-    def reset(self):
-        self.p = self.draw("p")
-        self.p += self.draw("p")
-        self.d = self.draw("d")
-        self.player_ace = False
-        self.dealer_ace = False
-        self.terminated = False
-        self.base_reward = 1
+        self.base_reward=1
         return self.get_state()
 
     def draw(self, participant="p"):
@@ -160,3 +85,4 @@ class BlackJackDouble:
             if self.p < self.d <= 21:
                 reward = -self.base_reward
         return self.get_state(), self.terminated, reward
+
